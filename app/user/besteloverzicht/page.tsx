f79ab517626b10765e056f5a82419ccfb1f6beae
Robin de Niet <accounts@cobeas.nl>
2024-08-04 22:21:08 +0200

    created websockets

--- app/user/besteloverzicht/page.tsx
+++ app/user/besteloverzicht/page.tsx
@@ -23,9 +23,36 @@ type TransformedOrder = {
 };
 
 export default function BestelOverzicht() {
+    const [data, setData] = useState<Order[]>([]);
+
+    useEffect(() => {
+        const wsUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL || "ws://localhost:8080";
+        const ws = new WebSocket(wsUrl);
+
+        ws.onopen = () => {
+            console.log("Websocket connected to ", wsUrl);
+        };
+
+        ws.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.event === 'open_orders') {
+                setData(message.data);
+            }
+        };
+
+        ws.onclose = () => {
+            console.log("Websocket closed");
+        };
+
+        return () => {
+            ws.close();
+        };
+    })
+
     return (
         <div>
             <h1>Besteloverzicht</h1>
+            <textarea style={{width: '100%', minHeight: '300px'}} value={JSON.stringify(data, null, 2)} readOnly />
         </div>
     );
 }
